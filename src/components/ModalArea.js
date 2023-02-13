1362905d41b900099c62ca18065e8a37b0fe36d0
choi <wjfekr1@naver.com>
2023-02-13 16:37:03 +0900

    init

---
+++ src/components/ModalArea.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ModalArea = () => {
+  return (
+    <div>
+      <button>모달1</button>
+      <button>모달2</button>
+    </div>
+  );
+};
+
+export default ModalArea;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ModalArea from "../components/ModalArea";
+
+const Home = () => {
+  return (
+    <div>
+      <ModalArea />
+    </div>
+  );
+};
+
+export default Home;
